7afc52449bf605b1192e99266dca99aa81b294a3
Leo <icpc0928@gmail.com>
2020-03-13 15:06:52 +0800

    20200312 Homework GuessNumbers

--- app/src/main/java/com/example/leosguessnum/MainActivity.java
+++ app/src/main/java/com/example/leosguessnum/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.leosguessnum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public MainActivity(){
+        Log.v("leo","MainAct");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
